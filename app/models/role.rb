c37af3fd30ff2e4e2695f6769592fd6eea42fdf3
Pete Fowler <fowler.pete@gmail.com>
2022-09-26 14:11:57 -0600

    Draft all methods

--- app/models/role.rb
+++ app/models/role.rb
@@ -13,6 +13,30 @@ class Role < ActiveRecord::Base
     arr
   end
 
+  def locations
+    arr = []
+    self.auditions.each do |a| 
+      arr << a.location
+    end
+    arr
+  end
 
+  def lead 
+    lead = self.auditions.where(hired == true).first
+    if lead == nil
+      'no actor has been hired for this role'
+    else
+      lead
+    end 
+  end
+
+  def understudy
+    under = self.auditions.where(hired == true).second
+    if under == nil
+      'no actor has been hired for understudy for this role'
+    else
+      under
+    end 
+  end
 
 end
